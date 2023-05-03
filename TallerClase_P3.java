f87f130f53de05fab713f8336999287c2ccbdabe
Cristian1609 <124215574+Cristian1609@users.noreply.github.com>
2023-05-03 19:52:06 -0400

    Add files via upload

---
+++ TallerClase_P3.java
@@ -0,0 +1,22 @@
+
+package tallerclase_p3;
+
+import javax.swing.JOptionPane;
+
+
+public class TallerClase_P3 {
+
+    
+    public static void main(String[] args) {
+      
+        Gerente gerente1 = new Gerente("Cristian");
+        Gerente gerente2 = new Gerente("Marcos", 1000);
+       
+        
+        System.out.println(gerente2.toString());
+
+       
+        
+    }
+    
+}
